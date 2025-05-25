2abeea1dad70b38ed91ad9b114d47b069884f7a3
Nicolly Eising <munhoznicolly@gmail.com>
2025-05-25 16:46:50 -0300

    Remove todos os arquivos do repositório

--- appcore/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import BoardDetailView, ApiRootView
-
-urlpatterns = [
-    path('', ApiRootView.as_view(), name='api-root'), 
-    path('api/board/<int:pk>/', BoardDetailView.as_view(), name='board-detail'),
-]
