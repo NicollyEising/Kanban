d9b5fa4a4e8e289fff86f5975a7df11c35aee0ec
Nicolly Eising <munhoznicolly@gmail.com>
2025-05-25 16:56:31 -0300

    Readiciona kanban-frontend como pasta comum (não submódulo)

---
+++ kanban-frontend/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <KanbanBoard />
+  </div>
+</template>
+
+<script>
+import KanbanBoard from './KanbanBoard.vue';
+
+export default {
+  name: 'App',
+  components: {
+    KanbanBoard,
+  },
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+</style>
